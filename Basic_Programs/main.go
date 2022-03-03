0757fa8dca0f849a088f4e5757bb8d1ab57e69de
Subhradeep Ray <subhradeepray2017@gmail.com>
2022-03-03 23:53:38 +0530

    Modified Project Structure

---
+++ Basic_Programs/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\nGoing Inside Hello World")
+	Hello()
+	fmt.Println("\nGoing Inside Variables")
+	Variables()
+}
---
+++ Basic_Programs/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Variables() {
+	//Variable Declaration With Initial Value
+	var employee1 = "Subhradeep"
+	var employee2 = "Ray"
+	employee3 := "Subhradeep Ray"
+	fmt.Println("Employee1 = " + employee1 + " Employee2 = " + employee2 + " Employee3 = " + employee3)
+}
